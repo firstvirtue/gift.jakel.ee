a82d0a9a12179d627cff1d4941521f86f3d2125b
firstvirtue <firstvirtue@gmail.com>
2018-07-13 03:11:47 +0900

    Add image/text

--- pages/ko/good-news.vue
+++ pages/ko/good-news.vue
@@ -20,17 +20,17 @@
         <h1 class="h1 heading">어린아이가 장난을 치다가 창문을 깼다면, 용서해 줄 수는 있지만 누군가 창문을 고쳐야 하지 않을까요?</h1>
       </div>
     </section>
-    <section class="fullpage-slide">
+    <section class="fullpage-slide white-tone">
       <div class="l-wrap">
         <h1 class="h1 heading">아마도 그 부모님이 대신 창문 값을 지불할 것입니다.</h1>
       </div>
     </section>
-    <section class="fullpage-slide">
+    <section class="fullpage-slide white-tone">
       <div class="l-wrap">
         <h1 class="h1 heading">이와 같습니다. 우리가 지불할 수 없는 죄 값을 하나님이 대신 치러 주신 것입니다.</h1>
       </div>
     </section>
-    <section class="fullpage-slide">
+    <section class="fullpage-slide white-tone">
       <div class="l-wrap">
         <h1 class="h1 heading">하지만 그 값은 창문처럼 값 싸지 않았습니다.</h1>
       </div>
@@ -58,7 +58,8 @@
         <h1 class="h1 heading">예수님은 십자가에 못 박혀 죽음으로 인간의 죄를 대신하셨죠.</h1>
       </div>
     </section>
-    <section class="fullpage-slide">
+    <section class="fullpage-slide sky">
+      <img class="fullpage-slide-back-img sky-back-img" src="~/assets/img/sky.jpg" alt="">
       <div class="l-wrap">
         <h1 class="h1 heading">그리고 나아가 예수님은 부활을 통해 죄로 인한 죽음의 저주를 완전히 끝내셨습니다.</h1>
       </div>
@@ -89,6 +90,29 @@ export default {
 
     .heading {
       top: 20%;
+    }
+  }
+
+  .wooden-cross {
+    &-back-img {
+      transform: translate(-10%, 0) scale(0.48);
+      transform-origin: 0 0;
+    }
+
+    .heading {
+      top: 36.3%;
+    }
+  }
+
+  .sky {
+    &-back-img {
+      transform: translate(-22%, -14%) scale(0.78);
+      transform-origin: 0 0;
+    }
+
+    .heading {
+      top: 63.3%;
+      color: $black;
     }
   }
 </style>
--- pages/ko/what-should-do.vue
+++ pages/ko/what-should-do.vue
@@ -1,34 +1,54 @@
 <template lang="html">
   <div class="wrap">
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">그렇다면 우리가 할 수 있는 것은 무엇일까요?</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">그렇다면 우리가 할 수 있는 것은 무엇일까요?</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">그것은 선택을 하는 것입니다.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">그것은 선택을 하는 것입니다.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">예수님이 내 죄를 위해 죽으시고 다시 사신 일을 믿는 것이 끊어진 하나님과의 관계에서 회복되는 유일한 길 입니다.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">예수님이 내 죄를 위해 죽으시고 다시 사신 일을 믿는 것이 끊어진 하나님과의 관계에서 회복되는 유일한 길 입니다.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">믿음으로 하나님과의 관계는 회복되고 다시 영원한 삶도 주어질 것입니다.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">믿음으로 하나님과의 관계는 회복되고 다시 영원한 삶도 주어질 것입니다.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">모든 것은 점점 비극 이전으로 되돌아갈 것입니다. 공허함도 불안감도 권태로움도 없었던 그 때로 말이죠.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">모든 것은 점점 비극 이전으로 되돌아갈 것입니다. 공허함도 불안감도 권태로움도 없었던 그 때로 말이죠.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">지금 당장 받아들이기 어려우실수도 있습니다. 하지만 좋습니다.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">지금 당장 받아들이기 어려우실수도 있습니다. 하지만 좋습니다.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">당신의 삶의 여정 가운데 또 어디선가 살고 있는 그리스도인이 당신에게 전해줄 것입니다.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">당신의 삶의 여정 가운데 또 어디선가 살고 있는 그리스도인이 당신에게 전해줄 것입니다.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">예수님을요</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">예수님을요</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">그 때면 좀 더 알게 되지 않을까요? 하나님께서는 당신을 향한 계획이 있으셨고, 항상 사랑하고 계셨다는 것을요.</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">그 때면 좀 더 알게 되지 않을까요? 하나님께서는 당신을 향한 계획이 있으셨고, 항상 사랑하고 계셨다는 것을요.</h1>
+      </div>
     </section>
-    <section class="fullpage-slide">
-      <h1 class="h1 heading">약속합니다. 예수님의 이름으로</h1>
+    <section class="fullpage-slide white-tone">
+      <div class="l-wrap">
+        <h1 class="h1 heading">약속합니다. <br>예수님의 이름으로</h1>
+      </div>
     </section>
   </div>
 </template>
