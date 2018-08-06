ac465da5ab3dfe61984054ebde5b9069cd8aad28
firstvirtue <firstvirtue@gmail.com>
2018-08-06 22:41:01 +0900

    Add deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+set -e
+
+npm run generate
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git remote add azure https://jakelee:joyfulWorks@brooklyn.scm.azurewebsites.net/brooklyn.git
+
+git pull azure
+git push -u azure master
+
+cd -
